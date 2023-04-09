402cfe5af7fa5849b738ce35b107223572f446dc
Rahma Rahadi <rahmalia.2002@gmail.com>
2023-04-10 01:20:31 +0700

    detail dan bikin folder layout dan pages

---
+++ resources/views/catalog/layout/footer.blade.php
@@ -0,0 +1,45 @@
+<!-- Footer start -->
+<footer class="mainfooter" role="contentinfo">
+    <div class="footer-middle">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                <!--Column1-->
+                    <div class="footer-pad">
+                        <h4>BANTUAN</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Hubungi Kami</a></li>
+                            <li><a href="#">FAQs</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">
+                    <!--Column1-->
+                    <div class="footer-pad">
+                        <img src="img/footer/logowhite.png">
+                        <ul class="list-unstyled">
+                            <li><a href="#">Tentang Kami</a></li>
+                            <li><a href="#">Sosial Media</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">
+                    <!--Column1-->
+                    <div class="footer-pad">
+                        <h4>Aplikasi Kami</h4>
+                        <div class="list-unstyled" >
+                            <h5>Download Furtable di Playstore dan dapatkan diskon melimpah.</h5>
+                        </div>
+                        <img src="img/footer/playstore.png">
+                    </div>
+                </div>
+            </div>
+            <div class="copy">
+                <div class="col-md-12 copy">
+                    <p class="text-center">&copy; Copyright 2023 - Furtable.  All rights reserved.</p>
+                </div>
+             </div>
+        </div>
+    </div>
+  </footer>
+<!-- Footer end -->
---
+++ resources/views/catalog/layout/navbar.blade.php
@@ -0,0 +1,24 @@
+<!-- Navbar start -->
+<nav class="navbar">
+    <div>
+        <a href="#" class="navbar-logo">
+            <img src="img/header/logo.png" alt="logo" width="30" height="24" class="d-inline-block align-text-top">
+            FURTABLE
+        </a>
+
+        <div class="navbar-nav">
+            <a href="/home">Home</a>
+            <a href="/product">Product</a>
+            <a href="/tentang">Tentang</a>
+        </div> 
+    </div>
+
+    <div class="navbar-extra">
+        <div class="search-box">
+            <a><img src="img/header/search.svg" /></a>
+            <input type="search" placeholder="Search" />
+        </div>
+        <a href="#" id="hamburger-menu" class="menu"><i data-feather="menu"></i></a>
+    </div>
+</nav>
+<!-- Navbar end -->
